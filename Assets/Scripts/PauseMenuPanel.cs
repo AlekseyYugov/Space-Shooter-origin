618a6973abee874d7adf227784874f113cc2bb6b
AlekseyYugov <67547259+AlekseyYugov@users.noreply.github.com>
2023-04-11 18:49:24 +0500

    Корректная работа паузы

---
+++ Assets/Scripts/PauseMenuPanel.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+namespace SpaceShooter
+{
+    public class PauseMenuPanel : MonoBehaviour
+    {
+        private void Start()
+        {
+            gameObject.SetActive(false);
+        }
+
+        public void OnButtonShowPause()
+        {
+            Time.timeScale = 0;
+            gameObject.SetActive(true);
+        }
+
+        public void OnButtonContinue()
+        {
+            if (LevelController.m_IsLevelCompleted)
+            {
+                Time.timeScale = 0;
+            }
+            else
+            {
+                Time.timeScale = 1;
+            }
+            
+            gameObject.SetActive(false);
+        }
+        public void OnButtonMainMenu()
+        {
+            Time.timeScale = 1;
+            gameObject.SetActive(false);
+            SceneManager.LoadScene(LevelSequenceController.MainMenuSceneNickname);
+        }
+    }
+}
+
